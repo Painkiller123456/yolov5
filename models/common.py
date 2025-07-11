5ca44f946f22d55970df501a3b0e1c754b2403c4
Painkiller123456 <panchofridayhaifudiyaan@gmail.com>
2025-07-11 19:34:37 +0530

    Update common.py
    Signed-off-by: Painkiller123456 <panchofridayhaifudiyaan@gmail.com>

--- models/common.py
+++ models/common.py
@@ -179,9 +179,9 @@ class Conv(nn.Module):
         """Initializes a standard convolution layer with optional batch normalization and activation."""
         super().__init__()
         if k == 3 and s == 1 and g == 1:
-            self.conv = WinogradConv2D(c1, c2,kernel_size=k,stride=s,padding=autopad(k, p),bias=False)
+            self.conv = WinogradConv2D(c1, c2, kernel_size=k, stride=s, padding=autopad(k, p), bias=False)
         else:
-            self.conv = nn.Conv2d(c1, c2, k, s,autopad(k, p),groups=g, bias=False)
+            self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
 
